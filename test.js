abcd07cbc9a6dd45f13cbf69ca491063f2aa9742
Bojidar <ivanov6@abv.bg>
2017-02-22 11:02:23 +0200

    Fix

--- test.js
+++ test.js
@@ -2,12 +2,14 @@ var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
 
+var port = process.env.PORT || 8080
+
 app.get('/',function(req,res){
     res.sendFile(__dirname + '/client/index.html');
 });
 app.use('/client',express.static(__dirname + '/client'));
 
-serv.listen(8080);
+serv.listen(port);
 console.log("Server started at localhost:8080");
 
 var SOCKET_LIST = {};
